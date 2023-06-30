b6c37347480b0d7521b83ae32c1add685d91bfe0
javdel3 <05JDcomputer@gmail.com>
2023-06-29 22:08:09 -0700

    Hero Image Slider

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
-// 10000 miliseconds = 10 seconds
-setInterval(function() {
-    console.log('jumbo move')
+const heroImage1;
+const heroImage2;
+const heroImage3;
+
+// Image Slider for Hero Section
+  // 10000 miliseconds = 10 seconds
+setInterval( () => {
+    heroImage1;
   },  
   10000
 );
